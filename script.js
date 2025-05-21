c04679dd7e526c29cd4ade3525e66d560b4858db
arpitklu <arrpiit2004@gmail.com>
2025-05-21 23:12:56 +0530

    javascript --> coonsole, arithmetic operations, conditions, operators done

---
+++ script.js
@@ -0,0 +1,8 @@
+console.warn("This is a warning")
+console.error("Not allowed if you are not human")
+alert("Humans only allowed")
+var conf=confirm('Are you Human?')
+console.log(conf)
+var naa= prompt("Whats ur name ??")
+console.log('UserName: '+naa)
+
