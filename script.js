904d1490eaa1a300c873e28c86c5cbf0e4e1da6d
arpitklu <arrpiit2004@gmail.com>
2025-05-22 12:28:16 +0530

    DataTypes, functions, loops in JS

--- script.js
+++ script.js
@@ -1,8 +1,84 @@
-console.warn("This is a warning")
-console.error("Not allowed if you are not human")
-alert("Humans only allowed")
-var conf=confirm('Are you Human?')
-console.log(conf)
-var naa= prompt("Whats ur name ??")
-console.log('UserName: '+naa)
+// console.warn("This is a warning")
+// console.error("Not allowed if you are not human")
+// alert("Humans only allowed")
+// var conf=confirm('Are you Human?')
+// console.log(conf)
+// var naa= prompt("Whats ur name ??")
+// console.log('UserName: '+naa)
 
+
+// // type-1
+// function abc(){
+//     console.log("Helloww!!")
+// }
+// abc()
+
+// //type-2
+// var def=function(){
+//     console.log("Hellooww!!")
+// }
+// def()
+
+// // type-3: ->  FAT ARROW FUNCTION.
+// var ghi=()=>{
+//     console.log("Helloww!!")
+// }
+// ghi()
+
+// var a=[1,2,'Arpiit','Kumar']
+// console.log(a)
+// a.push("Mishra")
+// console.log(a)
+// a.pop()
+// console.log(a)
+// console.log(a.length)
+
+
+//higher order functions:
+//(a function which demands a funcion during its callback)
+
+// var arr=[10,20,30,40,60,70]
+
+// arr.forEach(function(ele){
+//     console.log('Helloww!! ',ele)
+// })
+
+
+// map, filter, reduce, etc.
+
+//##################
+// () = Functions
+// {} = Objects
+// [] = Array
+//##################
+
+
+// OBJECTS
+
+
+var obj={
+    user:'Arpiit',
+    age:20
+}
+console.log(obj.user)
+console.log(obj.age)
+
+var obj1={
+    model:'iphone',
+    price:60000,
+    color:'white'
+}
+var obj2={
+    model:'samsung',
+    price:65000,
+    color:'blue'
+}
+var obj2={
+    model:'xiaomi',
+    price:50000,
+    color:'silver'
+}
+
+console.log(obj)
+console.log(obj1)
+console.log(obj2)
