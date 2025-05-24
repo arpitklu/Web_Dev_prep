e58b15175c73e460ba18ca4749eae871a064c66a
arpitklu <arrpiit2004@gmail.com>
2025-05-24 16:38:20 +0530

    react started

---
+++ react.js
@@ -0,0 +1,18 @@
+// ###########################################################################################
+
+
+// react js  is a library which is used to make UI
+// react DOM is used to connect/add the react library to your DOM(ex: your Browser)
+
+// WebApps: React + React DOM
+// MobileApps: React + React Native
+
+// ###########################################################################################
+
+
+// var root=document.querySelector("#root")
+// var h1=document.createElement('h1')
+// h1.innerHTML = "heyy"
+// root.appendChild(h1)
+
+console.log(React)
