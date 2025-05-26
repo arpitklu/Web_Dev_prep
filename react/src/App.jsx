00a0f9dd6fc22f38b32322e1bac8760e90744f95
arpitklu <arrpiit2004@gmail.com>
2025-05-26 23:40:56 +0530

    react, userState

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,9 +1,32 @@
-// rafce for boilerplate code
+// " rafce " for boilerplate code
+// "E:\WEB DEV\WebDev\react"
+// use "{}" to print the value of any constant.
+//################################################
 
-function App(){
+// hooks: special type of fuctions.
+// used for state management.
+
+// useState snippet:   const [first, setfirst] = useState(second)
+//################################################
+
+
+
+
+
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [Num, setNum] = useState()
   return (
-    <h1>Hello</h1>
+    <div>
+      <h1>The number is: {Num}</h1>
+      <button onClick={()=>setNum(Num+10)}>Increment</button>
+      <button onClick={()=>setNum(Num-10)}>Decrement</button>
+    </div>
   )
 }
 
 export default App
+
+
