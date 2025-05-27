5510a9e59bf40691f7a03fd01c601a326795fbfd
arpitklu <arrpiit2004@gmail.com>
2025-05-27 22:33:28 +0530

    tailwindcss

--- react2/react2/src/App.jsx
+++ react2/react2/src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
-
 import './App.css'
 import './index.css'
 
 import React from 'react'
 
+
 const App = () => {
   return (
     <div>
-      <h1 className='bg-red-200'>Arppit</h1>
+      <form >
+        <input className='px-3 font-semibold py-3 text-xl m-5 rounded' type="text" placeholder='Enter Your Name'/>
+        <button className='px-3 py-3 text-xl font-semibold bg-red-600 rounded'> Submit </button>
+      </form>
     </div>
   )
 }
