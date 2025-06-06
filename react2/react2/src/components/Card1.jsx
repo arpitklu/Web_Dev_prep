36530413c47826ef798c390f54ab3e4c21bc3287
arpitklu <arrpiit2004@gmail.com>
2025-06-06 21:49:40 +0530

    axios!!

--- react2/react2/src/components/Card1.jsx
+++ react2/react2/src/components/Card1.jsx
@@ -9,7 +9,7 @@ const Card1 = (props) => {
         <h1 className='text-2xl font-semibold mb-2'>{props.name}</h1>
         <h2 className='mb-2'>{props.city}, {props.age}</h2>
         <h3 className='mb-4'>{props.profession}</h3>
-        <button className= 'hover:cursor-pointer bg-emerald-700 text-white px-4 py-2 rounded text-medium'>Add User</button>
+        <button className= 'hover:cursor-pointer bg-emerald-700 text-white px-4 py-2 rounded text-medium active:scale-90'>Add User</button>
         </div>
     </>
   )
