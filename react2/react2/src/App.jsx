fa354e52e4b030cd619a66fe239f346f68994428
arpitklu <arrpiit2004@gmail.com>
2025-07-02 11:55:13 +0530

    axios!!

--- react2/react2/src/App.jsx
+++ react2/react2/src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-amber-700 m-4 p-3'>
-      <button>Click here</button>
-      
+    <div className='p-10 '>
+      <button className='bg-teal-700 text-white font-semibold text-2xl px-6 py-3 rounded active:scale-90'>Get Data</button>
+      <div className='p-5 bg-white '>Hello</div>
     </div>
   )
 }
