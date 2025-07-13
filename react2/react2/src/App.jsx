515404ba58a51b324ed61e3bdc1be2cd654db5b0
arpitklu <arrpiit2004@gmail.com>
2025-07-13 22:22:09 +0530

    Feat: axios

--- react2/react2/src/App.jsx
+++ react2/react2/src/App.jsx
@@ -1,21 +1,25 @@
 import React, { useState } from 'react'
-import axios from 'axios'
+import axios from 'axios';
+
 
 const App = () => {
 
-  
+  const [data, setdata] = useState([])
 
-  // const [isOpen, setIsOpen]= useState(false);
+  const getData= async ()=>{
 
-  const getData = async ()=>{
-
-    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=8')
-    console.log(response)
+    const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=10')
+    setdata(response.data)
+    // console.log(data)
   }
+  
   return (
-    <div className='p-10'>
-      <button onClick={getData} className='bg-teal-700 text-white font-semibold text-2xl active:scale-90 cursor-pointer'>Get Data</button>
-      
+    <div className='p-10 h-screen bg-black'>
+      <button onClick={getData} className='bg-red-600 text-white font-semibold text-2xl px-6 py-3 rounded active:scale-90'>get data</button>
+      <div className='p-5 bg-gray-700'></div>
+      {data.map(function(){
+        return <h1>Hello</h1>
+      })}
     </div>
   )
 }
